30534f525352f374fc5b95d38f83a31e46fce3ef
Michal Tkaczuk <michal_tkaczuk@wp.pl>
2021-11-15 22:33:25 +0100

    Global and local variables added in functions

--- test.js
+++ test.js
@@ -1,10 +1,17 @@
 function getAverage (a, b){
-  
+    
     var average = (a + b) / 2;
     console.log(average);
     return average;
 
 }
 
-var myResult = getAverage(7,48);
-console.log("The average is " + myResult);
+var myResult = getAverage(7, 11);
+
+function logResult(){
+
+    console.log("The average is " + myResult);
+
+}
+
+logResult();
