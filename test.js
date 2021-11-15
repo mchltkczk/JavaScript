9b08866ba3f9df45b5f7016e37895173d6d55b19
Michal Tkaczuk <michal_tkaczuk@wp.pl>
2021-11-15 16:52:56 +0100

    For loop added

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 var myAge = prompt("What is your age?");
 
-while (myAge < 18) {
+for (myAge; myAge < 18; myAge++) {
+    
     document.write("You can't come inside this club, you are " + myAge + "<br>");
-    myAge++;
+   
 }
 
 document.write("You are finally 18! You can come!");
