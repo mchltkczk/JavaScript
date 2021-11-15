13b0caecfaaea810441fbe87bb142bc4ba2ba6e8
Michal Tkaczuk <michal_tkaczuk@wp.pl>
2021-11-15 16:06:18 +0100

    if, else statements added

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-alert("Hello, World!");
+var youLikeMeat = true;
+var myNum = 11;
+
+if (myNum == 10) {
+
+    document.write("mNum is equal to 10");
+}   else {
+
+        document.write("myNum is not equal to 10");
+    
+}
