ccf5a8eab54b7dc14b13b0ee5dab97451280daed
Michal Tkaczuk <michal_tkaczuk@wp.pl>
2021-11-16 14:38:42 +0100

    Added array.pop() to return the Last Element in an Array

--- edabit_solutions.js
+++ edabit_solutions.js
@@ -1,13 +1,3 @@
-function isEvenOrOdd(num) {
-	
-	if (num % 2 == 0){
-		
-		let num = "even";
-		return num;
-
-	} else {
-		
-		let num = "odd";
-		return num;
-	}
+function getLastItem(arr) {
+	return arr.pop();
 }
